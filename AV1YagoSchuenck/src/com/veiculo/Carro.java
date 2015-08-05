498129ab6dc841415f611ef9e4b36e629e0be694
Yago <Yago@notasus-00>
2015-08-05 20:13:42 -0300

    Classes atualizadas

--- AV1YagoSchuenck/src/com/veiculo/Carro.java
+++ AV1YagoSchuenck/src/com/veiculo/Carro.java
@@ -1,5 +1,19 @@
 package com.veiculo;
 
-public class Carro {
-
+public class Carro extends Veiculo{
+	private float motorizacao;
+	private String cambio;
+	
+	public float getMotorizacao() {
+		return motorizacao;
+	}
+	public void setMotorizacao(float motorizacao) {
+		this.motorizacao = motorizacao;
+	}
+	public String getCambio() {
+		return cambio;
+	}
+	public void setCambio(String cambio) {
+		this.cambio = cambio;
+	}
 }
--- AV1YagoSchuenck/src/com/veiculo/Motocicleta.java
+++ AV1YagoSchuenck/src/com/veiculo/Motocicleta.java
@@ -1,5 +1,19 @@
 package com.veiculo;
 
-public class Motocicleta {
-
+public class Motocicleta extends Veiculo{
+	private int cilindrada;
+	private int capacidade;
+	
+	public int getCilindrada() {
+		return cilindrada;
+	}
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+	public int getCapacidade() {
+		return capacidade;
+	}
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}	
 }
